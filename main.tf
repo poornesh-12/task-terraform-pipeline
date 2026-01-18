e64111fd0b5c7303280ae7af4a0d82fdb521dece
poornesh-12 <poorneshkr24@gmail.com>
2026-01-18 14:05:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-jenkins-tf"
+  location = "East US"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "jenkins-tf-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      size,              # trial: ignore VM resize
+      tags               # ignore tag changes
+    ]
+  }
+}
